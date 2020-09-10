3297d710fc3a305fd8d12e99701432cd583699d5
Quenzell <61855248+Quenzell@users.noreply.github.com>
2020-09-10 12:51:50 +0700

    Update attendance.blade.php

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('title')
-    Attendance History
+    History
 @endsection
 
  @section('style')
@@ -29,43 +29,51 @@
         <div class="card o-hidden border-0 shadow-lg my-5" style="height: 500px;">
         {{-- navbar --}}
             <nav class="navbar navbar-light" style="background-color: #63D0FF; border-radius: 10px 10px 0px 0px;">
-            <a class="navbar-brand" href="history">
+            <a class="navbar-brand" href="/">
                 <img src="{{ url('/dashboard/dist/img/calendar.png') }}" width="30" height="30" class="d-inline-block align-top" alt="">
                 <b style="color: white; margin-left:20px;">Attendance History</b>
             </a>
             </nav>
-            <div class="container" style="margin-top: 10px;">
+
+        <div class="container" style="margin-top: 10px;">
         <div class="row">
           <div class="col-md">
-            <div class="card mb-4">
+            <div class="card mb-4" style="margin-left:20px; background-color: white;">
               <img src="{{ url('/dashboard/dist/img/1.png') }}" class="card-img-top">
+              <a href="attendance2" type="button" class="btn btn-primary btn-sm" style="height: 30px; width: 270px;"> Administrasi Server</a> 
+              <!-- <button type="button" class="btn btn-primary btn-sm" style="height: 30px; width: 270px;"> Administrasi Server</button> -->
             </div>
           </div>
           <div class="col-md">
-            <div class="card mb-4">
-              <img src="{{ url('/dashboard/dist/img/2.png') }}" class="card-img-top">
+            <div class="card mb-4" style="width: 280px; margin-left: 2px;">
+              <img src="{{ url('/dashboard/dist/img/2.png') }}" class="card-img-top" style="width: 280px; height: 170px;">
+              <button type="button" class="btn btn-primary btn-sm" style="height: 30px; width: 280px;"> Grafika Komputer</button>
             </div>
           </div>
           <div class="col-md">
-            <div class="card mb-4">
-              <img src="{{ url('/dashboard/dist/img/3.png') }}" class="card-img-top">
+            <div class="card mb-4" style="margin-right: 20px; height: 170px; ">
+              <img src="{{ url('/dashboard/dist/img/3.png') }}" class="card-img-top" style="height: 170px;">
+              <button type="button" class="btn btn-primary btn-sm" style="height: 30px; width: 265px;"> Jaringan Komputer Lanjut</button>
             </div>
           </div>
           <div class="contrainer">
             <div class="row">
-          <div class="col-md">
-            <div class="card mb-4">
-              <img src="{{ url('/dashboard/dist/img/4.png') }}" class="card-img-top">
+          <div class="col-md-4">
+            <div class="card mb-4" style="margin-left: 26px; height:200px; width:270px;">
+              <img src="{{ url('/dashboard/dist/img/4.png') }}" class="card-img-top" style="height: 170px;">
+              <button type="button" class="btn btn-primary btn-sm" style="height: 30px; width: 270px;"> Kewirausahaan Teknologi</button>
             </div>
           </div>
-          <div class="col-md">
-            <div class="card mb-4">
-              <img src="{{ url('/dashboard/dist/img/5.png') }}" class="card-img-top">
+          <div class="col-md-4">
+            <div class="card mb-4" style="margin-left: 10px; height: 170px; width:278px;">
+              <img src="{{ url('/dashboard/dist/img/5.png') }}" class="card-img-top" style="height:170px;">
+              <button type="button" class="btn btn-primary btn-sm" style="height: 30px; width: 280px;"> Pemograman Web</button>
             </div>
           </div>
-          <div class="col-md">
-            <div class="card mb-4">
-              <img src="{{ url('/dashboard/dist/img/6.png') }}"class="card-img-top">
+          <div class="col-md-4">
+            <div class="card mb-4" style="width: 280px; height:170px; margin-left: 5px;">
+              <img src="{{ url('/dashboard/dist/img/6.png') }}"class="card-img-top" style="height: 170px;">
+              <button type="button" class="btn btn-primary btn-sm" style="height: 30px; width: 280px;"> Pengembangan Aplikasi Mobile</button>
             </div>
           </div>
         </div>
@@ -74,6 +82,5 @@
       </div>
     </div>
 </div>
-
 
 @endsection
